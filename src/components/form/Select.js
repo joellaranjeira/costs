f2af05aa8ea673668bec166f3a8c544aaf1460e6
Joel da Rocha Laranjeira Junior <joellaranjeira@gmail.com>
2023-01-02 13:20:16 -0300

    aula 22 ajuste

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,11 +1,11 @@
 import styles from './Select.module.css'
 
-function Select ({ text, name, options, handleOnchange, value}){
+function Select({ text, name }) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name} >
-            <options>Selecione uma opção</options>
+                <options>Selecione uma opção</options>
             </select>
         </div>
     )
