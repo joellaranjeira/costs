11a0ef948d0da13ab8e9b60966d93cf4779b598c
Joel da Rocha Laranjeira Junior <joellaranjeira@gmail.com>
2023-01-07 10:03:19 -0300

    Aula 26 completa

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom"
 
+import styles from './Projects.module.css'
+
 import Message from "../layout/Message"
+import Container from '../layout/Container'
+import LinkButton from "../layout/LinkButton"
 
 function Projects() {
 
@@ -11,12 +15,21 @@ function Projects() {
     }
 
     return (
-        <div> <h1>Meus projetos</h1>
-            {message &&
-                <Message type="success" msg={message} />
-            }
-        </div>
+        <div className={styles.project_container}>
 
+            <div className={styles.title_container}>
+                <h1>Meus projetos</h1>
+                
+                <LinkButton to="/newprojects" text="Criar Projetos" />
+            </div>
+
+            {message && <Message type="success" msg={message} />}
+
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
+
+        </div>
     )
 
 }
